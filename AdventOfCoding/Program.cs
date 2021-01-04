f42bda019cdcd486ac5a08c11bb8cd4f97cf8a33
tstaszel <thomasstaszel@gmail.com>
2021-01-04 13:33:11 -0600

    Day 1 Part 1 of Advent of Code

---
+++ AdventOfCoding/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AdventOfCoding
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {   //We are using the base program to run our code for other classes
+            Day1Part1.Start();
+        }
+    }
+}
